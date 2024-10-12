5fb10956ff461b6ca30a5a3d36d37674f26c96c6
Vlad Bespalyy <Jawa0131@gmail.com>
2024-10-12 19:45:10 +0300

    fix progress1

--- src/Progress.jsx
+++ src/Progress.jsx
@@ -1,30 +1,16 @@
 import React from "react";
 
-const Progress = ({ seconds, minutes }) => {
-  const refPercent = useRef(100);
-  let resultTime = +seconds + minutes * 60;
-  let restWorkTimer = 0;
-  let percentOfSecond = 0;
-  let visibility = false;
-
-  if (resultTime) {
-    visibility = false
-    percentOfSecond = 100 / resultTime;
-    restWorkTimer = refPercent.current - percentOfSecond;
-    refPercent.current = restWorkTimer;
-  } else {
-    visibility = true;
-  }
+const Progress = ({ seconds, minutes, timeRef }) => {
+  const resultTime = +timeRef.current.seconds + timeRef.current.minutes * 60;
+  const countDownTime = +seconds + minutes * 60;
+  const percentOfCountDown = resultTime / 100;
+  const progress = 100 - countDownTime / percentOfCountDown;
 
   return (
     <>
-      <p>Прогресс</p>
+      <div>Прогресс</div>
       <div className="progress">
-        {visibility
-          ? "Задайте таймер"
-          : restWorkTimer >= 0
-          ? restWorkTimer + percentOfSecond
-          : 0}
+        {progress >= 0 ? `${Math.floor(progress)} %` : 100 + " %"}
       </div>
     </>
   );
